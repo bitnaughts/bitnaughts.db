3f5196332ad225605dd238081dcb4538be1f262d
Brian <brianhungerman@gmail.com>
2019-09-12 18:11:16 -0700

    Init DB Handler

---
+++ Handlers/DatabaseHandler.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+public class DatabaseHandler {
+
+    /* 
+        Handles all local and cloud data fetching
+    */
+
+    const bool FETCH_LOCALLY = true;
+
+    public DatabaseHandler() {
+        
+    }
+
+
+}
