f404fbcfb0a3e5a072a96b1619e41189e157fbe6
Brian <brianhungerman@gmail.com>
2019-10-16 01:43:08 -0700

    Add Player Table to test

---
+++ Constructors/dbo.Players.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Players] (
+    [player_id]          INT          NOT NULL,
+    [alias]              VARCHAR (50) NULL,
+    [password]           VARCHAR (50) NULL,
+    [current_ship_id]    INT          NULL,
+    [current_session_id] INT          NULL,
+    PRIMARY KEY CLUSTERED ([player_id] ASC)
+);
+
