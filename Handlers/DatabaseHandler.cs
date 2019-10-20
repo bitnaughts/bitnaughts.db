435aa989318d8057d1eae86da7cb03a3a214e5c9
Brian <brianhungerman@gmail.com>
2019-10-19 20:14:07 -0700

    Post function, Add Galaxy example

--- Handlers/DatabaseHandler.cs
+++ Handlers/DatabaseHandler.cs
@@ -1,17 +1,43 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-public class DatabaseHandler {
+public static class DatabaseHandler {
 
     /* 
         Handles all local and cloud data fetching
     */
 
-    const bool FETCH_LOCALLY = true;
+    private static readonly HttpClient client = new HttpClient ();
 
-    public DatabaseHandler() {
-        
+    public const string URL = "https://bitnaughts.azurewebsites.net/api/";
+
+    public static void InitializeClient () {
+        // if (client == null) client ;
     }
 
+    public static async Task<string> Post (string endpoint, Dictionary<string, string> parameters) {
+        
+        InitializeClient ();
 
+        var content = new FormUrlEncodedContent (parameters);
+        var response = await client.PostAsync (URL + endpoint, content);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public static bool AddGalaxy (GalaxyObject galaxy) {
+        Post (
+            "add/galaxy",
+            new Dictionary<string, string> { 
+                { "id", galaxy.seed.ToString() },
+                { "seed", galaxy.seed.ToString() }
+            }
+        );
+        return true;
+    }
+    
 }
