572eb6ec678ddb280fa112cbfa0f306a09d4cfc2
Brian <brianhungerman@gmail.com>
2019-10-21 09:50:09 -0700

    Adding fields to Systems table

--- Constructors/dbo.Systems.sql
+++ Constructors/dbo.Systems.sql
@@ -1,5 +1,7 @@
 ﻿CREATE TABLE [dbo].[Systems]
 (
-	[s_system_id] INT PRIMARY KEY, 
-    [s_seed] INT NULL
+    [s_system_id] INT PRIMARY KEY,
+    [s_seed] INT NOT NULL,
+    [s_position_x] DECIMAL(5,2) NOT NULL,
+    [s_position_y] DECIMAL(5,2) NOT NULL
 )
